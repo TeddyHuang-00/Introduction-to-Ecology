b57addef625f9cd3a639d159626871ed031460e4
TeddyHuang-00 <huang_nan_2019@pku.edu.cn>
2023-01-10 03:53:18 +0800

    Add batch replication

--- batch.py
+++ batch.py
@@ -3,6 +3,7 @@ import json
 import subprocess
 
 python_ver = "python3.11"
+rep_seeds = [0, 1, 2]
 default_config = {
     "name": "default",
     "prey": [500, 500],
@@ -20,48 +21,54 @@ default_config = {
 configs = [default_config | c for c in json.loads(open("./batch-config.json").read())]
 
 for config in configs:
-    print(config)
-    if (
-        os.path.exists(os.path.join("fig", config["name"], "Population.mp4"))
-        and os.path.exists(os.path.join("fig", config["name"], "Distance.mp4"))
-        and not config.get("force", False)
-    ):
-        print("Output directory exists, skipping")
-        continue
-    subprocess.call(
-        [
-            python_ver,
-            "simulate.py",
-            "-P",
-            *map(str, config["prey"]),
-            "-T",
-            *map(str, config["toxicity"]),
-            "-H",
-            str(config["predator"]),
-            "-G",
-            str(config["genes"]),
-            "-N",
-            str(config["generations"]),
-            "-R",
-            str(config["mutation_rate"]),
-            "-K",
-            str(config["mutation_scale"]),
-            "-I",
-            str(config["initial_scale"]),
-            "-S",
-            str(config["seed"]),
-            "-C",
-            config["cmap"],
-            "-O",
-            os.path.join("fig", config["name"]),
-        ]
-    )
-    subprocess.call(
-        [
-            python_ver,
-            "post-process.py",
-            "-F",
-            "-D",
-            os.path.join("fig", config["name"]),
-        ]
-    )
+    for s in rep_seeds:
+        config["seed"] = s
+        print(config)
+        if (
+            os.path.exists(
+                os.path.join("fig", config["name"] + f"_S{s}", "Population.mp4")
+            )
+            and os.path.exists(
+                os.path.join("fig", config["name"] + f"_S{s}", "Distance.mp4")
+            )
+            and not config.get("force", False)
+        ):
+            print("Output directory exists, skipping")
+            continue
+        subprocess.call(
+            [
+                python_ver,
+                "simulate.py",
+                "-P",
+                *map(str, config["prey"]),
+                "-T",
+                *map(str, config["toxicity"]),
+                "-H",
+                str(config["predator"]),
+                "-G",
+                str(config["genes"]),
+                "-N",
+                str(config["generations"]),
+                "-R",
+                str(config["mutation_rate"]),
+                "-K",
+                str(config["mutation_scale"]),
+                "-I",
+                str(config["initial_scale"]),
+                "-S",
+                str(config["seed"]),
+                "-C",
+                config["cmap"],
+                "-O",
+                os.path.join("fig", config["name"] + f"_S{s}"),
+            ]
+        )
+        subprocess.call(
+            [
+                python_ver,
+                "post-process.py",
+                "-F",
+                "-D",
+                os.path.join("fig", config["name"] + f"_S{s}"),
+            ]
+        )
